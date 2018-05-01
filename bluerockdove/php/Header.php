e3d7916be834fc2067a369a21ca7d87a925c39a9
MichelleFern <38120515+MichelleFern@users.noreply.github.com>
2018-05-01 02:06:53 -0700

    Blue Rock Dove
    Group project - create a website for a client

---
+++ bluerockdove/php/Header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Header</title>
+</head>
+<a href="https://www.facebook.com/" title="follow me to facebook"><img src="img/facebook.png"  alt="facebook link" width="36" height="34" class="fb"></a>
+  <div class="profileLogo"> 
+    <!-- Profile logo. -->
+    <p class="logoPlaceholder"> <img src="../img/bluerockdove.png" alt="Blue Rock Dove Adornments Logo"> </p>
+  </div>
+  
+   <!-- Profile Header -->
+  <section class="profileHeader">
+    <img src="img/bluerockdove.png" alt=""><!--<h1>Blue Rock Dove Adornments</h1>--></section>
+<body>
+</body>
+</html>
---
+++ bluerockdove/php/footer.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Footer</title>
+</head>
+
+<body>
+<footer>
+  <hr>
+  <p class="footerDisclaimer">2017  Copyrights - <span>All Rights Reserved</span></p>
+  <p class="footerNote">Sandy Abril - <span><a href="mailto:Sandyabril@aol.com">Sandyabril@aol.com</a></span></p>
+</footer>
+</body>
+</html>
